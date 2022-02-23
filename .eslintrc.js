20d091a8d919362a48fd40cc27758629b1312a1e
Steven Goodstein <sgoodstein@medallia.com>
2022-02-23 11:59:52 -0500

    ES lint sync test

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     browser: true,
     commonjs: true,
     es2021: true,
+    jest: true,
   },
   extends: [
     'airbnb-base',
@@ -10,9 +11,15 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
   },
+  plugins: ['jest'],
   rules: {
     'max-len': 0,
     'no-console': 0,
     'consistent-return': 0,
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
   },
 };
