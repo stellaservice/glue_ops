6d1a1b2871a71dda69f0c63dd1e6225946709ef9
Steven Goodstein <sgoodstein@medallia.com>
2022-03-03 12:01:46 -0500

    Update version to: 1.0.2

--- update-version.sh
+++ update-version.sh
@@ -11,6 +11,6 @@ fi
 glue_ops sync -c ./update_version.glueops.yaml -r version=$new_version
 
 git commit -am "Update version to: $new_version"
-git push
 git tag v$new_version
+git push
 git push origin v$new_version
