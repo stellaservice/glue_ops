278c68447ae398d67650c5872a452f3a59196c43
Steven Goodstein <sgoodstein@medallia.com>
2022-03-08 16:29:09 -0500

    Wrap cli in try catch

--- glue_ops.js
+++ glue_ops.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 /* eslint-disable no-unused-expressions */
+/* eslint-disable global-require */
 
 const yargs = require('yargs');
 
@@ -15,12 +16,17 @@ const defaultOptions = {
   },
 };
 
-yargs
-  .usage('$0 <cmd> [args] \n\n The glue for GitOps')
-  .scriptName('glue_ops')
-  .command(require('./cmds/run'))
-  .command(require('./cmds/sync'))
-  .command(require('./cmds/template'))
-  .option(defaultOptions.configPathOption)
-  .demandCommand()
-  .argv;
+try {
+  yargs
+    .usage('$0 <cmd> [args] \n\n The glue for GitOps')
+    .scriptName('glue_ops')
+    .command(require('./cmds/run'))
+    .command(require('./cmds/sync'))
+    .command(require('./cmds/template'))
+    .option(defaultOptions.configPathOption)
+    .demandCommand()
+    .argv;
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
