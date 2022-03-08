62c525a0cca2ef0529175c65d297538bf2ea1eeb
Steven Goodstein <sgoodstein@medallia.com>
2022-03-08 15:57:34 -0500

    Refactor sync tests, test runAllSyncs

--- src/sync.test.js
+++ src/sync.test.js
@@ -1,57 +1,72 @@
-const fs = require('fs');
-const YAML = require('yaml');
-const { runSync } = require('./sync');
+const fs = require('fs-extra');
+const readYamlFile = require('../test/utils/readYamlFile');
+const { runSync, runAllSyncs } = require('./sync');
 
-describe('runSync', () => {
+describe('run', () => {
   const fixturePath = 'test/fixtures';
 
-  describe('yaml target files', () => {
-    const tmpFilePath = '/tmp/test-sync.yaml';
+  describe('runSync', () => {
+    describe('yaml target files', () => {
+      const tmpFilePath = '/tmp/test-sync.yaml';
 
-    beforeEach(() => {
-      const testFilePath = `${fixturePath}/test_file.yaml`;
-      const file = fs.readFileSync(testFilePath);
-      fs.writeFileSync(tmpFilePath, file);
+      beforeEach(() => {
+        fs.copySync(`${fixturePath}/test_file.yaml`, tmpFilePath);
+      });
+
+      test('it syncs the image tag via yaml sync', () => {
+        const configFilePath = `${fixturePath}/glue_ops_file_sync_yaml.fixture.yaml`;
+        const fileSync = readYamlFile(configFilePath).fileSyncs.UpdateWebImage;
+
+        runSync(fileSync);
+
+        const testFile = readYamlFile(tmpFilePath);
+        expect(testFile.image.tag).toBe('syncedTag');
+      });
+
+      test('it syncs the image repo via regex sync', () => {
+        const configFilePath = `${fixturePath}/glue_ops_file_sync_regex.fixture.yaml`;
+        const fileSync = readYamlFile(configFilePath).fileSyncs.UpdateWebImage;
+
+        runSync(fileSync);
+
+        const testFile = readYamlFile(tmpFilePath);
+        expect(testFile.image.tag).toBe('syncedTag');
+      });
     });
 
-    test('it syncs the image tag via yaml sync', () => {
-      const configFilePath = `${fixturePath}/glue_ops_file_sync_yaml.fixture.yaml`;
-      const fileSync = YAML.parse(fs.readFileSync(configFilePath, 'utf-8')).fileSyncs.UpdateWebImage;
+    describe('json target files', () => {
+      const tmpFilePath = '/tmp/test-sync.json';
 
-      runSync(fileSync);
+      beforeEach(() => {
+        fs.copySync(`${fixturePath}/test_file.json`, tmpFilePath);
+      });
 
-      const testFile = YAML.parse(fs.readFileSync(tmpFilePath, 'utf-8'));
-      expect(testFile.image.tag).toBe('syncedTag');
-    });
+      test('it syncs the image tag via json sync', () => {
+        const configFilePath = `${fixturePath}/glue_ops_file_sync_json.fixture.yaml`;
+        const fileSync = readYamlFile(configFilePath).fileSyncs.UpdateWebImage;
 
-    test('it syncs the image repo via regex sync', () => {
-      const configFilePath = `${fixturePath}/glue_ops_file_sync_regex.fixture.yaml`;
-      const fileSync = YAML.parse(fs.readFileSync(configFilePath, 'utf-8')).fileSyncs.UpdateWebImage;
+        runSync(fileSync);
 
-      runSync(fileSync);
-
-      const testFile = YAML.parse(fs.readFileSync(tmpFilePath, 'utf-8'));
-      expect(testFile.image.tag).toBe('syncedTag');
+        const testFile = JSON.parse(fs.readFileSync(tmpFilePath, 'utf-8'));
+        expect(testFile.image.tag).toBe('syncedTag');
+      });
     });
   });
 
-  describe('json target files', () => {
-    const tmpFilePath = '/tmp/test-sync.json';
+  describe('runAllSyncs', () => {
+    const tmpFilePath = '/tmp/test-sync.yaml';
 
     beforeEach(() => {
-      const testFilePath = `${fixturePath}/test_file.json`;
-      const file = fs.readFileSync(testFilePath, 'utf-8');
-      fs.writeFileSync(tmpFilePath, file);
+      fs.copySync(`${fixturePath}/test_file.yaml`, tmpFilePath);
     });
 
-    test('it syncs the image tag via json sync', () => {
-      const configFilePath = `${fixturePath}/glue_ops_file_sync_json.fixture.yaml`;
-      const fileSync = YAML.parse(fs.readFileSync(configFilePath, 'utf-8')).fileSyncs.UpdateWebImage;
+    it('calls runSync with each file sync', () => {
+      const config = readYamlFile(`${fixturePath}/glue_ops_file_sync_multi.yaml`);
+      runAllSyncs(config);
 
-      runSync(fileSync);
-
-      const testFile = JSON.parse(fs.readFileSync(tmpFilePath, 'utf-8'));
+      const testFile = readYamlFile(tmpFilePath);
       expect(testFile.image.tag).toBe('syncedTag');
+      expect(testFile.foo.bar).toBe('foobar');
     });
   });
 });
